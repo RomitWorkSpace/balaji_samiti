88de380b15f3e3bb612ff83e6712dc019bf79d39
RomitWorkSpace <romitkumar848@gmail.com>
2024-08-29 20:19:27 +0530

    Event page design completed

---
+++ resources/views/front/event.blade.php
@@ -0,0 +1,72 @@
+@extends('layout.front-layout')
+
+@section('content')
+
+<div class="container-fluid p-0 relative">
+    <img src="/images/banners/event.png" alt="Balaji banner" width="100%" />
+    <div class="banner-title-right">
+        <h3 class="bold-6">OUR</h3>
+        <h3 class="bold-6">EVENTS</h3>
+    </div>
+</div>
+
+    <div class="container text-center mt-4 pt-5 pb-5">
+      <h3 class="bold-6">आगामी कार्यक्रम</h3>
+      <p class="sub-title text-center">के उद्देश्य को लेकर परमात्मा की बनाई हुई...............</p>
+    </div>
+
+<div class="upcomming-event">
+    <div class="container-fluid event-wrapper">
+    
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="event-card bottom">
+                        <img src="/images/event1.png" alt="" width="100%" />
+                        <div class="event-content">
+                             संस्कार प्रतियोगिता<br/>
+                            सनातन को जानें<br/>
+                            आश्वनि कृष्ण पक्ष द्वादशी <br/>
+                            29 सितंबर 2024 (रविवार)
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="event-card bottom">
+                        <img src="/images/event2.png" alt="" width="100%" />
+                        <div class="event-content">
+                             संस्कार प्रतियोगिता<br/>
+                            सनातन को जानें<br/>
+                            आश्वनि कृष्ण पक्ष द्वादशी <br/>
+                            29 सितंबर 2024 (रविवार)
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="event-card bottom">
+                        <img src="/images/event1.png" alt="" width="100%" />
+                        <div class="event-content">
+                             संस्कार प्रतियोगिता<br/>
+                            सनातन को जानें<br/>
+                            आश्वनि कृष्ण पक्ष द्वादशी <br/>
+                            29 सितंबर 2024 (रविवार)
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="event-card bottom">
+                        <img src="/images/event2.png" alt="" width="100%" />
+                        <div class="event-content">
+                             संस्कार प्रतियोगिता<br/>
+                            सनातन को जानें<br/>
+                            आश्वनि कृष्ण पक्ष द्वादशी <br/>
+                            29 सितंबर 2024 (रविवार)
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
