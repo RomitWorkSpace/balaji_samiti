690ab13036ea65ba74be7be8f7d32449446a1653
RomitWorkSpace <romitkumar848@gmail.com>
2024-08-29 20:18:44 +0530

    cause page desing completed

---
+++ resources/views/front/cause.blade.php
@@ -0,0 +1,61 @@
+@extends('layout.front-layout')
+
+@section('content')
+
+<div class="container-fluid p-0 relative">
+    <img src="/images/banners/cause.png" alt="Balaji banner" width="100%" />
+    <div class="banner-title-right">
+        <p class="white sub-title bold-6">Each Donation is an Essential Help</p>
+        <p class="white sub-title bold-6">Which improves everyone's life</p>
+    </div>
+</div>
+
+<div class="container-fluid pt-5 pb-5">
+      <div class="container" data-aos="fade-up">
+        <div class="row">
+          <div class="col-md-4">
+            <div class="message-item relative">
+              <div class="relative">
+                <img src="/images/mes1.png" alt="image" width="100%" class="" />
+                <div class="message-btn">DONATE NOW</div>
+              </div>
+              <div class="message-text box-shadow p-4">
+                <p class="sub-title">के उद्देश्य को लेकर परमात्मा की बनाई हुई...............</p>
+                <p class="sub-title">के उद्देश्य को लेकर परमात्मा की बनाई हुई...............</p>
+              </div>
+              <div class="message-btn2">View Detail</div>
+            </div>
+          </div>
+
+          <div class="col-md-4">
+          <div class="message-item relative">
+              <div class="relative">
+                <img src="/images/mes2.png" alt="image" width="100%" class="" />
+                <div class="message-btn">DONATE NOW</div>
+              </div>
+              <div class="message-text box-shadow p-4">
+                <p class="sub-title">के उद्देश्य को लेकर परमात्मा की बनाई हुई...............</p>
+                <p class="sub-title">के उद्देश्य को लेकर परमात्मा की बनाई हुई...............</p>
+              </div>
+              <div class="message-btn2">View Detail</div>
+            </div>
+          </div>
+          
+          <div class="col-md-4">
+            <div class="message-item relative">
+              <div class="relative">
+                <img src="/images/mes1.png" alt="image" width="100%" class="" />
+                <div class="message-btn">DONATE NOW</div>
+              </div>
+              <div class="message-text box-shadow p-4">
+                <p class="sub-title">के उद्देश्य को लेकर परमात्मा की बनाई हुई...............</p>
+                <p class="sub-title">के उद्देश्य को लेकर परमात्मा की बनाई हुई...............</p>
+              </div>
+              <div class="message-btn2">View Detail</div>
+            </div>
+          </div>
+</div>
+      </div>
+    </div>
+
+@endsection
